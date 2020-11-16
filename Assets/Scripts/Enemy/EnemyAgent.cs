8b84568a5eb072e380438ee0ce3e51bfda3cdd8a
hageyobigun <skei081518@gmail.com>
2020-11-16 18:22:50 +0900

    保存

--- Assets/Scripts/Enemy/EnemyAgent.cs
+++ Assets/Scripts/Enemy/EnemyAgent.cs
@@ -8,52 +8,33 @@ using System;
 
 public class EnemyAgent : Agent, Enemy.IAttackable
 {
-    [SerializeField] private int death = 0 ;
+    [SerializeField] private int hpValue = 100;
+    [SerializeField] private int mpValue = 100;
 
-    [SerializeField] private int hpValue = 1;
-    [SerializeField] private int maxMpValue = 100;
-    private int mpValue;
-
-    [SerializeField] private int _enemyPos = 4;
     [SerializeField] private GameObject player = null;
     private int[] moveList = { -3, -1, 1, 3, 0};
 
     private EnemyMove _enemyMove ;
     private EnemyAttacks _enemyAttacks;
     private PlayerAgent _playerAgent;
+    private SliderModel _sliderModel;
 
-    protected bool IsDead() => --hpValue <= 0;
-
-    //イベントを発行する核となるインスタンス
     private Subject<int> attackSubject = new Subject<int>();
-
-    //イベントの購読側だけを公開
-    public IObservable<int> OnAttackAction
-    {
-        get { return attackSubject; }
-    }
-
-    private Subject<Unit> mpSubject = new Subject<Unit>();
-
     private Subject<int> moveSubject = new Subject<int>();
 
     //プロパティー
     public int GetHpValue
     {
-        get { return this.hpValue; }  //取得用
-        private set { this.hpValue = value; } //値入力用
-    }
-
-    public void Attacked(float damage)
-    {
-        IsDead();
+        get { return this._sliderModel.hp.Value; }  //取得用
+        private set { this._sliderModel.hp.Value = value; } //値入力用
     }
 
     public override void Initialize()
     {
-        _enemyMove = new EnemyMove(_enemyPos, gameObject);
+        _enemyMove = new EnemyMove(4, gameObject);
         _enemyAttacks = GetComponent<EnemyAttacks>();
         _playerAgent = player.GetComponent<PlayerAgent>();
+        _sliderModel = GetComponent<SliderModel>();
 
         attackSubject
             .Where(attack => attack == 1)
@@ -61,36 +42,31 @@ public class EnemyAgent : Agent, Enemy.IAttackable
             .Subscribe(_ => _enemyAttacks.BulletAttack());
 
         attackSubject
-            .Where(attack => attack == 2 && mpValue >= 3)
+            .Where(attack => attack == 2 && _sliderModel.mp.Value >= 3)
             .ThrottleFirst(TimeSpan.FromSeconds(0.5f))
             .Subscribe(_ =>
             {
                 _enemyAttacks.FireAttack();
-                mpValue -= 3;
+                _sliderModel.mp.Value -= 3;
             });
 
         attackSubject
-            .Where(attack => attack == 3 && mpValue >= 4)
+            .Where(attack => attack == 3 && _sliderModel.mp.Value >= 4)
             .ThrottleFirst(TimeSpan.FromSeconds(0.7f))
             .Subscribe(_ =>
             {
                 _enemyAttacks.BombAttack();
-                mpValue -= 4;
+                _sliderModel.mp.Value -= 4;
             });
 
         attackSubject
-            .Where(attack => attack == 4 && mpValue >= 5)
+            .Where(attack => attack == 4 && _sliderModel.mp.Value >= 5)
             .ThrottleFirst(TimeSpan.FromSeconds(2.5f))
             .Subscribe(_ =>
             {
                 StartCoroutine(_enemyAttacks.BarrierGuard());
-                mpValue -= 5;
+                _sliderModel.mp.Value -= 5;
             });
-
-        mpSubject
-            .Where(_ => mpValue <= maxMpValue)
-            .ThrottleFirst(TimeSpan.FromSeconds(1.0f))
-            .Subscribe(_ => mpValue++);
 
         moveSubject
             .Where(move => _enemyMove.IsStage(moveList[move]))
@@ -101,8 +77,7 @@ public class EnemyAgent : Agent, Enemy.IAttackable
     //エピソード開始時
     public override void OnEpisodeBegin()
     {
-        hpValue = 10;
-        mpValue = maxMpValue;
+        _sliderModel.Initialize(hpValue, mpValue);
     }
 
 
@@ -110,7 +85,8 @@ public class EnemyAgent : Agent, Enemy.IAttackable
     {
         sensor.AddObservation(this.transform.position);
         sensor.AddObservation(player.transform.position);
-        sensor.AddObservation(mpValue);
+        sensor.AddObservation(_sliderModel.hp.Value);
+        sensor.AddObservation(_sliderModel.mp.Value);
     }
 
     public override void OnActionReceived(float[] vectorAction)
@@ -123,20 +99,23 @@ public class EnemyAgent : Agent, Enemy.IAttackable
 
         if (_playerAgent != null)
         {
-            if (_playerAgent.GetHpValue <= 0)
+            if (_playerAgent.GetHpValue <= 0) //撃破
             {
                 AddReward(1.0f);
                 EndEpisode();
             }
         }
 
-        if (hpValue < 0)
+        if (_sliderModel.hp.Value < 0)//死亡
         {
             AddReward(0.2f);
-            death++;
             EndEpisode();
         }
-        mpSubject.OnNext(Unit.Default);
     }
 
+
+    public void Attacked(float damage)
+    {
+        _sliderModel.hp.Value -= (int)damage;
+    }
 }
--- Assets/Scripts/Player/PlayerAgent.cs
+++ Assets/Scripts/Player/PlayerAgent.cs
@@ -8,46 +8,26 @@ using System;
 
 public class PlayerAgent : Agent , Player.IAttackable
 {
-    [SerializeField] private int death = 0;
-
-    [SerializeField] private int hpValue = 1;
+    [SerializeField] private int hpValue = 100;
+    [SerializeField] private int mpValue = 100;
     [SerializeField] private GameObject enemy = null;
-    [SerializeField] private int maxMpValue = 100;
-    private int mpValue;
 
     private int[] moveList = { -3, -1, 1, 3, 0};
 
     private PlayerMove _playerMove;
     private PlayerStage _playerStage;
     private PlayerAttack _playerAttack;
-
     private EnemyAgent _enemyAgent;
+    private SliderModel _sliderModel;
 
-    protected bool IsDead() => --hpValue <= 0;
-
-    //イベントを発行する核となるインスタンス
     private Subject<int> attackSubject = new Subject<int>();
-
-    //イベントの購読側だけを公開
-    public IObservable<int> OnAttackAction
-    {
-        get { return attackSubject; }
-    }
-
-    private Subject<Unit> mpSubject = new Subject<Unit>();
-
     private Subject<int> moveSubject = new Subject<int>();
 
     //プロパティー
     public int GetHpValue
     {
-        get { return this.hpValue; }  //取得用
-        private set { this.hpValue = value; } //値入力用
-    }
-
-    public void Attacked(float damage)
-    {
-        IsDead();
+        get { return this._sliderModel.hp.Value; }  //取得用
+        private set { this._sliderModel.hp.Value = value; } //値入力用
     }
 
     public override void Initialize()
@@ -56,6 +36,7 @@ public class PlayerAgent : Agent , Player.IAttackable
         _playerStage = new PlayerStage(4);
         _playerAttack = GetComponent<PlayerAttack>();
         _enemyAgent = enemy.GetComponent<EnemyAgent>();
+        _sliderModel = GetComponent<SliderModel>();
 
         attackSubject
             .Where(attack => attack == 1)
@@ -63,36 +44,31 @@ public class PlayerAgent : Agent , Player.IAttackable
             .Subscribe(_ => _playerAttack.BulletAttack());
 
         attackSubject
-            .Where(attack => attack == 2 && mpValue >= 3)
+            .Where(attack => attack == 2 && _sliderModel.mp.Value >= 3)
             .ThrottleFirst(TimeSpan.FromSeconds(0.7f))
             .Subscribe(_ =>
             {
                 _playerAttack.FireAttack();
-                mpValue -= 3;
+                _sliderModel.mp.Value -= 3;
             });
 
         attackSubject
-            .Where(attack => attack == 3 && mpValue >= 4)
+            .Where(attack => attack == 3 && _sliderModel.mp.Value >= 4)
             .ThrottleFirst(TimeSpan.FromSeconds(0.7f))
             .Subscribe(_ =>
             {
                 _playerAttack.BombAttack();
-                mpValue -= 4;
+                _sliderModel.mp.Value -= 4;
             });
 
         attackSubject
-            .Where(attack => attack == 4 && mpValue >= 5)
+            .Where(attack => attack == 4 && _sliderModel.mp.Value >= 5)
             .ThrottleFirst(TimeSpan.FromSeconds(2.5f))
             .Subscribe(_ =>
             {
                 StartCoroutine(_playerAttack.BarrierGuard());
-                mpValue -= 5;
+                _sliderModel.mp.Value -= 5;
             });
-
-        mpSubject
-            .Where(_ => mpValue <= maxMpValue)
-            .ThrottleFirst(TimeSpan.FromSeconds(1.0f))
-            .Subscribe(_ => mpValue++);
 
         moveSubject
             .Where(move => _playerStage.IsStage(moveList[move]))
@@ -103,8 +79,7 @@ public class PlayerAgent : Agent , Player.IAttackable
     //エピソード開始時
     public override void OnEpisodeBegin()
     {
-        hpValue = 10;
-        mpValue = maxMpValue;
+        _sliderModel.Initialize(hpValue, mpValue);
     }
 
 
@@ -112,7 +87,8 @@ public class PlayerAgent : Agent , Player.IAttackable
     {
         sensor.AddObservation(this.transform.position);
         sensor.AddObservation(enemy.transform.position);
-        sensor.AddObservation(mpValue);
+        sensor.AddObservation(_sliderModel.hp.Value);
+        sensor.AddObservation(_sliderModel.mp.Value);
     }
 
     public override void OnActionReceived(float[] vectorAction)
@@ -131,13 +107,16 @@ public class PlayerAgent : Agent , Player.IAttackable
                 EndEpisode();
             }
         }
-        if (hpValue < 0)
+        if (_sliderModel.hp.Value < 0)
         {
             AddReward(0.2f);
-            death++;
             EndEpisode();
         }
-        mpSubject.OnNext(Unit.Default);
+    }
+
+    public void Attacked(float damage)
+    {
+        _sliderModel.hp.Value -= (int)damage;
     }
 
 }

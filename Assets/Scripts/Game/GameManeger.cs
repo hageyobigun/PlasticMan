b2fcb863701a5d6c2f76142d4965d885f21e7cca
hageyobigun <skei081518@gmail.com>
2020-11-19 23:37:12 +0900

    no message

--- Assets/Scripts/Game/GameManeger.cs
+++ Assets/Scripts/Game/GameManeger.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManeger : MonoBehaviour
+public class GameManeger : SingletonMonoBehaviour<GameManeger>
 {
-    // Start is called before the first frame update
+    public enum GameState
+    {
+        Start,
+        Playing,
+        GameOver
+    }
+    private GameState currentGameState;
+
     void Start()
     {
+        currentGameState = GameState.Start;
+    }
+
+    public void SetCurrentState(GameState state)
+    {
+        currentGameState = state;
+        OnGameStateChanged(currentGameState);
+    }
+
+    // 状態が変わったら何をするか
+    void OnGameStateChanged(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.Start:
+                GameTitle();
+                break;
+            case GameState.Playing:
+                GameStart();
+                break;
+            case GameState.GameOver:
+                GameOver();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+
+    void GameTitle()
+    {
+        SceneManager.LoadScene("Start");
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameStart()
     {
-        
+        SceneManager.LoadScene("Play");
     }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
 }

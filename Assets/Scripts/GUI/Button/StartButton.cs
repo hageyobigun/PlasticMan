c569ff68d66550628cbaeb90969185d31695d975
hageyobigun <skei081518@gmail.com>
2021-02-02 00:07:44 +0900

    画面調整

--- Assets/Scripts/GUI/Button/StartButton.cs
+++ Assets/Scripts/GUI/Button/StartButton.cs
@@ -12,13 +12,9 @@ using UnityEngine.UI;
 public class StartButton : MonoBehaviour
 {
     [SerializeField] private float time = 2.0f;
-    [SerializeField] private TextMeshProUGUI _textMeshPro = null;
+    [SerializeField] private TextMeshProUGUI _textMeshPro = default;
     //[SerializeField] private Image whiteImage = null;
-    [SerializeField] private Image blackBack = default;
-    [SerializeField] private GameObject canvas = default ;
-    private List<Image> backList = new List<Image>();
-    //16:9
-    //-375 375 200 -200  50が基準
+    //[SerializeField] private Image blackBack = default;
 
     void Start()
     {
@@ -34,42 +30,29 @@ public class StartButton : MonoBehaviour
             {
                 tween.Kill(); //停止
                 SoundManager.Instance.PlaySe("TitleButton"); //サウンド
-                //MoveScene();
-                //StartCoroutine(MoveScene());
                 GameManeger.Instance.currentGameStates.Value = GameState.Start;
-
-
                 //画面遷移
                 //whiteImage.DOFade(1, 1.0f)
                 //.OnComplete(() => GameManeger.Instance.currentGameStates.Value = GameState.Start);
             });
     }
 
-
-    private IEnumerator MoveScene()
-    {
-        var oneBlockScale = blackBack.rectTransform.sizeDelta;
-        for (int x = -375; x <= 375; x += (int)oneBlockScale.x)
-        {
-            for (int y = -200; y <= 200; y += (int)oneBlockScale.x)
-            {
-                var copy = Instantiate(blackBack, new Vector3(x, y, 0), Quaternion.identity);
-                copy.transform.SetParent(canvas.transform, false);
-                copy.gameObject.SetActive(false);
-                backList.Add(copy);
-            }
-        }
-
-        while (backList.Count > 0)
-        {
-            int index = Random.Range(0, backList.Count);
-            backList[index].gameObject.SetActive(true);
-            backList.RemoveAt(index);
-            yield return new WaitForSeconds(0.003f);
-        }
-        yield return new WaitForSeconds(0.6f);
-        GameManeger.Instance.currentGameStates.Value = GameState.Start;
-    }
+    //16:9
+    //-375 375 200 -200  50が基準
+    //private void InstanceBlock()
+    //{
+    //    var oneBlockScale = blackBack.rectTransform.sizeDelta;
+    //    for (int x = -375; x <= 375; x += (int)oneBlockScale.x)
+    //    {
+    //        for (int y = -200; y <= 200; y += (int)oneBlockScale.x)
+    //        {
+    //            var copy = Instantiate(blackBack, new Vector3(x, y, 0), Quaternion.identity);
+    //            copy.transform.SetParent(canvas.transform, false);
+    //            copy.gameObject.SetActive(false);
+    //            backList.Add(copy);
+    //        }
+    //    }
+    //}
 
 
 }
--- Assets/Scripts/Game/Maneger/MoveScene.cs
+++ Assets/Scripts/Game/Maneger/MoveScene.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MoveScene
